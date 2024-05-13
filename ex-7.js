ffa081680be883d5c3e6ae30039ba6838066d8d2
Nat Tan <tresarpnat27@gmail.com>
2024-05-13 14:00:04 +0700

    resolv exercise array js

--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,16 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+let isArray = Array.isArray(orders);
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+let productQuantityOfJoannet = orders[3].productQuantity;
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+
+console.log(isArray);
+console.log(creditCardTypeOfBlindermann);
+orders[2].creditCardType = "visa";
+console.log(orders[2].creditCardType);
+console.log(productQuantityOfJoannet);
+console.log(totalPurchaseOfDary);
+orders.shift();
+console.log(orders);
